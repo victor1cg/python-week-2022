e79708065a1c090103eee695ff6a5e36214fe653
Victor Gonçalves <victor1cg@hotmail.com>
2022-06-19 23:32:57 -0300

    .

--- beerlog/models.py
+++ beerlog/models.py
@@ -1,10 +1,24 @@
-from dateclasses import dataclass
+from dataclasses import dataclass
 #dateclass é utilizado para guardar dados.
 
 #definindo os tipos consigo realizar uma validação
 #@dataclass é um decorador, injetar novos metodos e classes; Cria automatica// um inicializador __init__
 @dataclass
-class Beer:
+class Beer2:
+    name: str
+    style: str
+    flavor: int
+    imagem: int
+    cost: int
+
+#Criar um objeto que se pareça com um tabela
+from typing import Optional             #ajuda a criar o id aleatorio
+from sqlmodel import SQLModel, Field
+from sqlmodel import select
+from pydantic import validator
+
+class Beer(SQLModel, table = True):
+    id: Optional[int] =Field(primary_key=True, default = None)     #Obrigatorio ter id para o sql
     name: str
     style: str
     flavor: int
