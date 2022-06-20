9b29206380886e8d618b6bd644de24bed3b62f1e
Victor Gonçalves <victor1cg@hotmail.com>
2022-06-20 16:30:57 -0300

    ok

---
+++ beerlog/database.py
@@ -0,0 +1,9 @@
+#SqLite
+from sqlmodel import create_engine          #cria um objeto capaz de se conectar com um DB
+from beerlog.config import settings
+from beerlog import models                  #tabela da classe beer seja criada    
+
+
+engine = create_engine(settings.database.url)  #engine conectada ao db de arquivos
+
+models.SQLModel.metadata.create_all(engine)   #cria a tabela do banco de dados
--- beerlog/models.py
+++ beerlog/models.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass
-#dateclass é utilizado para guardar dados.
+# dateclass é utilizado para guardar dados.
 
-#definindo os tipos consigo realizar uma validação
-#@dataclass é um decorador, injetar novos metodos e classes; Cria automatica// um inicializador __init__
+# definindo os tipos consigo realizar uma validação
+# @dataclass é um decorador, injetar novos metodos e classes; Cria automatica// um inicializador __init__
+
 
 @dataclass
 class Beer2:
@@ -12,20 +13,24 @@ class Beer2:
     imagem: int
     cost: int
 
-#-------------
-#Criar um objeto que se pareça com um tabela
+# -------------
+# Criar um objeto que se pareça com um tabela
 from typing import Optional             #ajuda a criar o id aleatorio
 from sqlmodel import SQLModel, Field
 from sqlmodel import select
 from pydantic import validator          #validador, por exemplo o sabor de 1-10
+from statistics import mean             #biblioteca para calcular media
+from datetime import datetime
 
-class Beer(SQLModel, table = True):
+class Beer(SQLModel, table = True) :
     id: Optional[int] = Field(primary_key=True, default = None)      #Obrigatorio ter id para o sql.
     name: str                                                       #temos os atributos e os metodos
     style: str
     flavor: int
     image: int
     cost: int
+    rate: int=0                                                     #adicionar o rate, com default 0
+    date: datetime = Field(default_factory=datetime.now)
 
     @validator("flavor","image","cost")                             #aplicar o mesmo design pattern.DECORATOR
     def validating_ratings(cls, v, field):                          #função associada a essa classe.
@@ -33,4 +38,12 @@ class Beer(SQLModel, table = True):
             raise RuntimeError(f'\n{field.name} must be between one and ten')
         return v
 
-brewdog = Beer(name='Brewdog', style='NEIPA', flavor=60, image=8, cost=10)
+    @validator ('rate',always = True)
+    def calculate_rate(cls,v,values):
+        rate = mean(
+            [values['flavor'],values['image'],values['cost']]
+            )
+        return int(rate)
+
+
+brewdog = Beer(name='Brewdog', style='NEIPA', flavor=6, image=8, cost=10)
